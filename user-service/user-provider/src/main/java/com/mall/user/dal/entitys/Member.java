99e5699aba4fb686c4363b82c61c270d3c7d1bd2
Chee_Tung <chee_tung@163.com>
2020-06-13 12:52:29 +0800

    Lucas_Tung

--- user-service/user-provider/src/main/java/com/mall/user/dal/entitys/Member.java
+++ user-service/user-provider/src/main/java/com/mall/user/dal/entitys/Member.java
@@ -1,6 +1,7 @@
 package com.mall.user.dal.entitys;
 
 import lombok.*;
+import org.checkerframework.checker.units.qual.C;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
@@ -20,54 +21,64 @@ public class Member {
     /**
      * 用户名
      */
+    @Column
     private String username;
 
     /**
      * 密码，加密存储
      */
+    @Column
     private String password;
 
     /**
      * 注册手机号
      */
+    @Column
     private String phone;
 
     /**
      * 注册邮箱
      */
+    @Column
     private String email;
 
+    @Column
     private Date created;
 
+    @Column
     private Date updated;
 
+    @Column
     private String sex;
-
+    @Column
     private String address;
-
+    @Column
     private Integer state;
 
     /**
      * 头像
      */
+    @Column
     private String file;
-
+    @Column
     private String description;
 
     /**
      * 积分
      */
+    @Column
     private Integer points;
 
     /**
      * 余额
      */
+    @Column
     private Double balance;
 
     /**
      *  是否激活,默认值N，激活Y
      */
-    @Column(name = "isVerified")
+    @Column(name = "isverified")
     private String isVerified;
 
 
